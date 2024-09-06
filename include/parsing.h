4b66314ffd0078d895ad20cc589a16dd72bc9332
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2024-09-06 19:56:19 +0530

    Add files via upload

--- include/parsing.h
+++ include/parsing.h
@@ -1,7 +1,8 @@
 #include <stdint.h>
+#include <stddef.h>
 
 // function to parse sbus channel
-uint16_t *parse_buffer(uint8_t buff[]);
+uint16_t *parse_buffer(uint8_t buff[], size_t buff_size, uint16_t channel[]);
 
 // interpolate channel and return pwm value 
 int interpolation(uint16_t ch);
